51b0a4aeefd8f902ec15634864385c718bf75f51
BigboyBartus <113651476+BigboyBartus@users.noreply.github.com>
2023-09-12 19:05:59 -0700

    Add files via upload

---
+++ screens/ProfileScreen.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {Text, View, TouchableOpacity, StyleSheet} from 'react-native'
+
+
+
+export default class ProfileScreen extends Component{
+render(){
+    return(
+        <View>
+            <Text>
+                Profile Screen!
+            </Text>
+        </View>
+    )
+}
+
+}
